339569a4c6f4f7ee7eb61a86fdacd308a5904c6b
dawn <164216919+v3a5@users.noreply.github.com>
2026-03-06 10:09:57 +0000

    Update cass-app.js

--- js/cass-app.js
+++ js/cass-app.js
@@ -1,49 +1,21 @@
-function showDashboard(){
+let repo;
 
-document.getElementById("skills").innerHTML="";
+function connectServer(){
 
-}
+let ip = document.getElementById("serverIP").value;
 
-function clearSkills(){
+localStorage.setItem("cassServer",ip);
 
-document.getElementById("skills").innerHTML="";
-
-}
-
-async function loadSkills(){
-
-const container=document.getElementById("skills");
-
-container.innerHTML="Loading framework...";
-
-try{
-
-const response=await fetch("frameworks/framework.json");
-
-const data=await response.json();
-
-container.innerHTML="";
-
-data.competencies.forEach(skill=>{
-
-const card=document.createElement("div");
-
-card.className="skill-card";
-
-card.innerHTML=
-"<h3>"+skill.name+"</h3>"+
-"<p>"+skill.description+"</p>";
-
-container.appendChild(card);
-
-});
-
-}catch(err){
-
-container.innerHTML="Failed to load framework";
-
-console.error(err);
+window.location = "dashboard.html";
 
 }
+
+function getRepo(){
+
+repo = new EcRepository();
+
+repo.selectedServer = localStorage.getItem("cassServer");
+
+return repo;
 
 }
